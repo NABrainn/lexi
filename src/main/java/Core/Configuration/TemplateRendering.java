0cc44c4fba9882ee570a1486ca7bf2661bf147be
nabrain <michal2002.60@gmail.com>
2026-08-08 10:32:54 +0200

    improve app mode resolution in temp engine config

--- src/main/java/Core/Configuration/TemplateRendering.java
+++ src/main/java/Core/Configuration/TemplateRendering.java
@@ -5,16 +5,26 @@ import gg.jte.TemplateEngine;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.config.JavalinConfig;
 import io.javalin.rendering.template.JavalinJte;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 
 public class TemplateRendering {
+
+    public static final Logger logger = LoggerFactory.getLogger(TemplateRendering.class);
+
     public static void configure(JavalinConfig config) {
         final var ENV = Dotenv.load();
-        var isDev = Boolean.parseBoolean(ENV.get("DEV"));
-        var templateEngine = isDev
-                ? JavalinJte.Companion.directoryTemplateEngine()
-                : TemplateEngine.createPrecompiled(Path.of("target/jte-classes"), ContentType.Html);
+        var mode = ENV.get("MODE");
+        var templateEngine = switch(mode) {
+            case "DEV" -> JavalinJte.Companion.directoryTemplateEngine();
+            case "PROD" -> TemplateEngine.createPrecompiled(Path.of("target/jte-classes"), ContentType.Html);
+            default -> {
+                logger.info("Failed to resolve value of MODE in .env, falling back TemplateEngine config to DEV");
+                yield JavalinJte.Companion.directoryTemplateEngine();
+            }
+        };
         var jte = new JavalinJte(templateEngine);
         config.fileRenderer(jte);
     }
